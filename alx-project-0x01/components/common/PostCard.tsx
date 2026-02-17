a90e89276858d2062e189d3818811dc00b879ecd
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2026-02-17 10:45:57 +0300

    Update PostCard.tsx

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,10 @@
+const PostCard: React.FC = () => {
+  return (
+    <div className="p-4 bg-white shadow rounded-lg">
+      <h2 className="text-xl font-bold">Post Title</h2>
+      <p>Post content</p>
+    </div>
+  );
+};
+
+export default PostCard;
