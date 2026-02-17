2e7435ddeb81725638fb8ffb69912d1644585f8f
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2026-02-17 10:50:01 +0300

    Update index.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,14 +1,39 @@
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 
-const Posts: React.FC = () => {
+const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
-        <h1 className="text-2xl font-semibold">Posts Page</h1>
+        <h1 className="text-2xl font-semibold mb-4">Post Content</h1>
+
+        <div className="grid grid-cols-3 gap-4">
+          {posts.map((post) => (
+            <PostCard
+              key={post.id}
+              title={post.title}
+              body={post.body}
+              userId={post.userId}
+              id={post.id}
+            />
+          ))}
+        </div>
       </main>
     </div>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts = await response.json();
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
 
 export default Posts;
