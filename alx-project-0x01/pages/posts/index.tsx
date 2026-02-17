f3ce702104b6baa12612de8cf8d432935393ece0
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2026-02-17 11:02:54 +0300

    Update index.tsx

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,26 +1,50 @@
-import PostCard from "@/components/common/PostCard";
+import { useState } from "react";
 import Header from "@/components/layout/Header";
-import { PostProps } from "@/interfaces";
+import PostCard from "@/components/common/PostCard";
+import PostModal from "@/components/common/PostModal";
+import { PostData, PostProps } from "@/interfaces";
 
 const Posts: React.FC<{ posts: PostProps[] }> = ({ posts }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+  const [post, setPost] = useState<PostData | null>(null);
+
+  const handleAddPost = (newPost: PostData) => {
+    setPost({ ...newPost, id: posts.length + 1 });
+  };
+
   return (
     <div className="flex flex-col h-screen">
       <Header />
       <main className="p-4">
-        <h1 className="text-2xl font-semibold mb-4">Post Content</h1>
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-2xl font-semibold">Post Content</h1>
+          <button
+            onClick={() => setModalOpen(true)}
+            className="bg-blue-700 px-4 py-2 rounded-full text-white"
+          >
+            Add Post
+          </button>
+        </div>
 
         <div className="grid grid-cols-3 gap-4">
-          {posts.map((post) => (
+          {posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
             <PostCard
-              key={post.id}
-              title={post.title}
-              body={post.body}
-              userId={post.userId}
-              id={post.id}
+              title={title}
+              body={body}
+              userId={userId}
+              id={id}
+              key={key}
             />
           ))}
         </div>
       </main>
+
+      {isModalOpen && (
+        <PostModal
+          onClose={() => setModalOpen(false)}
+          onSubmit={handleAddPost}
+        />
+      )}
     </div>
   );
 };
